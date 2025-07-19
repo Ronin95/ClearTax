729a549de0a837f63d4ffb89cceeba1f121c8db6
Nikola <Ronin95@users.noreply.github.com>
2025-07-19 14:24:21 +0200

    working on landing page and node.js deployment to VPS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,26 +12,40 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import { PieChart } from '@mui/x-charts/PieChart';
+import Avatar from '@mui/material/Avatar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import FaceIcon from '@mui/icons-material/Face';
 
 function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
+  todo: string,
+  areaOfInvestment: string
 ) {
-  return { name, calories, fat, carbs, protein };
+  return { todo, areaOfInvestment };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Pothole has to be filled.', 'Infrastructure'),
+  createData('Invest in chinese high-speed trains and bring them to Austria.', 'Technology'),
+  createData('We need more busses passing through in this village.', 'Transportation'),
+  createData('Replace broken swings and slides at Stadtpark Kinder playground', 'Infrastructure'),
+  createData('Build covered bike lanes connecting Linz suburbs to downtown', 'Transportation'),
 ];
 
+function generate(element: React.ReactElement<unknown>) {
+  return [0, 1, 2].map((value) =>
+    React.cloneElement(element, {
+      key: value,
+    }),
+  );
+}
+
 function App() {
+  const [dense] = React.useState(false);
+  const [secondary] = React.useState(false);
+
   return (
     <>
       <header className='app-header'>
@@ -62,47 +76,37 @@ function App() {
         <section className="features-container">
           <Stack spacing={2} direction="row">
             <div className="feature-box">
-              <p>Live Table displaying how people are using their taxes.</p>
-              <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Dessert (100g serving)</TableCell>
-                      <TableCell align="right">Calories</TableCell>
-                      <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                      <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                      <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {rows.map((row) => (
-                      <TableRow
-                        key={row.name}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                      >
-                        <TableCell component="th" scope="row">
-                          {row.name}
-                        </TableCell>
-                        <TableCell align="right">{row.calories}</TableCell>
-                        <TableCell align="right">{row.fat}</TableCell>
-                        <TableCell align="right">{row.carbs}</TableCell>
-                        <TableCell align="right">{row.protein}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
+              <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+                Live List displaying how people are using their taxes.
+              </Typography>
+              <List dense={dense}>
+              {generate(
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <FaceIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="has supported"
+                    secondary={secondary ? 'Secondary text' : null}
+                  />
+                </ListItem>,
+              )}
+            </List>
             </div>
             
             <div className="feature-box">
-              <p>What aspects have citizens supported the most.</p>
+              <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+                What aspects have citizens supported the most.
+              </Typography>
               <PieChart
                 series={[
                   {
                     data: [
                       { id: 0, value: 10, label: 'Infrastructure' },
-                      { id: 1, value: 15, label: 'Education' },
-                      { id: 2, value: 20, label: 'Healthcare' },
+                      { id: 1, value: 15, label: 'Technology' },
+                      { id: 2, value: 20, label: 'Transportation' },
                     ],
                   },
                 ]}
@@ -112,31 +116,29 @@ function App() {
             </div>
 
             <div className="feature-box">
-              <p>Examine where you can help further.</p>
+              <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+                Examine where you can help further.
+              </Typography>
               <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
                   <TableHead>
                     <TableRow>
-                      <TableCell>Dessert (100g serving)</TableCell>
-                      <TableCell align="right">Calories</TableCell>
-                      <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                      <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                      <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                      <TableCell>To Do</TableCell>
+                      <TableCell align="right">Area of Investment</TableCell>
                     </TableRow>
                   </TableHead>
                   <TableBody>
                     {rows.map((row) => (
                       <TableRow
-                        key={row.name}
+                        key={row.todo}
                         sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                       >
                         <TableCell component="th" scope="row">
-                          {row.name}
+                          {row.todo}
                         </TableCell>
-                        <TableCell align="right">{row.calories}</TableCell>
-                        <TableCell align="right">{row.fat}</TableCell>
-                        <TableCell align="right">{row.carbs}</TableCell>
-                        <TableCell align="right">{row.protein}</TableCell>
+                        <TableCell align="right">
+                          {row.areaOfInvestment}
+                        </TableCell>
                       </TableRow>
                     ))}
                   </TableBody>
@@ -144,6 +146,21 @@ function App() {
               </TableContainer>
             </div>
           </Stack>
+        </section>
+
+        <section className='reviews'>
+          <Avatar>H</Avatar>
+          <Stack spacing={2} direction="column">
+            <Typography className="hero-title" variant="h2" gutterBottom>
+              Welcome to ClearTax
+            </Typography>
+            <Typography className="hero-subtitle" variant="h3" gutterBottom>
+              ClearTax gives you control over how your tax money helps society. 
+              Don't rely on politicians to represent your interests - it's your money, 
+              you should decide how it's used and benefit society through your own actions!
+            </Typography>
+          </Stack>
+
         </section>
       </main>
     </>
